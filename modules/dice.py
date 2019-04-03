9d5404045b58807be67384a3bb3bd951155bb89d
Carl <carl.amko@twosixlabs.com>
2019-04-03 17:31:22 -0400

    Spencer added in d10 and d12
    * Fixed not catching invalid dice choices

--- modules/dice.py
+++ modules/dice.py
@@ -8,18 +8,21 @@ async def r(ctx):
 	dice = msg.content
 
 	async def invalid():
-		await bot.say("Invalid usage. Must use format dX. (X=4,6,8,20)")
+		await bot.say("Invalid usage. Must use format dX. (X=4,6,8,10,12,20)")
 
 	# Attempt to see if input is in the valid format.
 	try:
-		dice = dice[dice.index('d') + 1]
+		dice = dice[(dice.index('d') + 1):]
 	except ValueError:
 		await invalid()
 		return
 
 	res = roll_die(int(dice))
 
-	await bot.say(f"{msg.author.mention}\nRolling d{dice}: **{res}**")
+	if res:
+		await bot.say(f"{msg.author.mention}\nRolling d{dice}: **{res}**")
+	else:
+		await invalid()
 
 
 @bot.command(pass_context=True)
@@ -29,7 +32,7 @@ async def rr(ctx):
 	print(parts)
 
 	if not parts[1] or not parts[1].split('d'):
-		await bot.say("Incorrect usage. Must use format NdX. (N=0+, X=4,6,8,20)")
+		await bot.say("Incorrect usage. Must use format NdX. (N=0+, X=4,6,8,10,12,20)")
 	else:
 
 		for _ in range(parts[1].split('d')[0]):
@@ -37,7 +40,7 @@ async def rr(ctx):
 
 
 def roll_die(sides):
-	valid_dice = [4, 6, 8, 20]
+	valid_dice = [4, 6, 8, 10, 12, 20]
 
 	# Must be a valid dice choice.
 	if sides not in valid_dice:
