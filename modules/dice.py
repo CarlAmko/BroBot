a6d092467519a56770e1ad6f3fd8b9cbd9a7c396
Carl <carl.amko@twosixlabs.com>
2019-04-10 06:34:05 -0400

    Reworked dice module to use Regex to capture dice input
    * Merged dice commands into 1

--- modules/dice.py
+++ modules/dice.py
@@ -1,53 +1,50 @@
-from bot import bot
 import random
+import re
+
+from bot import bot
+
+valid_dice = [4, 6, 8, 10, 12, 20]
 
 
 @bot.command()
 async def r(ctx):
 	msg = ctx.message
-	dice = msg.content
+	text = msg.content
 
 	async def invalid():
-		await bot.say("Invalid usage. Must use format dX. (X=4,6,8,10,12,20)")
+		await ctx.send("Invalid usage. Must use format NdX. (N=0+, X=4,6,8,10,12,20)")
 
-	# Attempt to see if input is in the valid format.
-	try:
-		dice = dice[(dice.index('d') + 1):]
-	except ValueError:
+	# Check if input matches expected dice pattern
+	matches = re.findall(r"([\s0-9])d(\d{1,2})", text) or None
+	if matches:
+		dice_str = matches[0]
+		print(dice_str)
+	else:
 		await invalid()
 		return
 
-	res = roll_die(int(dice))
-	if res:
-		await ctx.send(f"{msg.author.mention}\nRolling d{dice}: **{res}**")
-	else:
+	num_dice, sides = dice_str[0], int(dice_str[1])
+	# Must be a valid dice choice.
+	if sides not in valid_dice:
 		await invalid()
+		return
 
+	# Cast string to int.
+	num_dice = int(num_dice) if num_dice.strip() else 1
 
-@bot.command()
-async def fuckhead(ctx):
-	await ctx.send("Happy Birthday Spencer!")
+	print(f"Rolling {num_dice} d{sides}...")
+	# Assemble roll result.
+	total = 0
+	msg = f"{msg.author.mention}\n"
+	for _ in range(num_dice):
+		val = roll_die(sides)
+		total += val
+		msg += f"d{sides}: **{val}**\n"
+	msg += f"Total: **{total}**"
 
-
-@bot.command()
-async def rr(ctx):
-	msg = ctx.message
-	parts = msg.split()
-	print(parts)
-
-	if not parts[1] or not parts[1].split('d'):
-		await ctx.send("Incorrect usage. Must use format NdX. (N=0+, X=4,6,8,10,12,20)")
-	else:
-		for _ in range(parts[1].split('d')[0]):
-			await ctx.send(f"{msg.author}\n")
+	await ctx.send(msg)
 
 
 def roll_die(sides):
-	valid_dice = [4, 6, 8, 10, 12, 20]
-
-	# Must be a valid dice choice.
-	if sides not in valid_dice:
-		return
-
 	# Return a random value from 1-X
 	return random.randint(1, sides)
