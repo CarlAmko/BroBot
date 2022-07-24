6d3d2333f14760666c5f07f4f8dfa05082de1878
Carl Amko <carl@carlamko.me>
2022-07-24 12:49:00 -0600

    wordle games are now instanced per person

--- modules/wordle.py
+++ modules/wordle.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import random
+from typing import Dict, Optional, List
 
 import emoji
 from discord import Member
@@ -11,10 +12,9 @@ from modules import bot
 
 MAX_GUESSES = 6
 
-num_guesses_used = 0
-word_to_guess = None
-chars_remaining = {}
-prev_guess_stack = []
+words: Dict[int, Optional[str]] = {}
+guesses_used: Dict[int, int] = {}
+prev_guesses: Dict[int, List[str]] = {}
 
 wrong_guess = emoji.emojize(':black_large_square:')
 correct_guess = emoji.emojize(':green_square:')
@@ -26,85 +26,90 @@ WORD_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data/words.json')
 # noinspection PyTypeChecker
 @bot.command()
 async def wordle(ctx: Context):
-    global word_to_guess, num_guesses_used, prev_guess_stack
+	global words, guesses_used, prev_guesses
 
-    if word_to_guess is not None:
-        await ctx.send(f"{ctx.author.mention} Game is already started.")
-    else:
-        with open(WORD_DATA_PATH, 'r') as data_file:
-            words = json.load(data_file)
-            while word_to_guess is None:
-                candidate = random.choice(words)
-                if candidate['score'] >= 1000 and ' ' not in candidate['word']:
-                    word_to_guess = candidate['word']
-                    # word_to_guess = 'brobot'
-            num_guesses_used = 0
-            prev_guess_stack = []
-            await ctx.send('Wordle game started! Type !gw to guess a word or !clearwordle to stop a game in progress.')
+	author = ctx.author
+	if author.id in words:
+		await ctx.send(f"{ctx.author.mention} Game is already started.")
+	else:
+		with open(WORD_DATA_PATH, 'r') as data_file:
+			word_json = json.load(data_file)
+			while author.id not in words:
+				candidate = random.choice(word_json)
+				if candidate['score'] >= 1000 and ' ' not in candidate['word']:
+					words[author.id] = candidate['word']
+			guesses_used[author.id] = 0
+			prev_guesses[author.id] = []
+			await ctx.send(
+				f'{author.mention} Wordle game started! Type !gw to guess a word or !clearwordle to stop a game in progress.'
+			)
 
 
 @bot.command()
 async def gw(ctx: Context):
-    global word_to_guess, num_guesses_used, chars_remaining
+	global words, guesses_used, prev_guesses
+	author: Member = ctx.author
+	key: int = author.id
 
-    if not word_to_guess:
-        await ctx.send("World game has not started. Type !wordle to start one.")
-    else:
-        guess: str = ctx.message.content.split(" ", 1)[1].lower()
-        # Validate guesses
-        author: Member = ctx.author
-        if len(guess) != 6:
-            await ctx.send(f"{author.mention} Guesses must be 6 characters")
-        elif not guess.isalpha():
-            await ctx.send(f"{author.mention} Guesses must contain only alphabetic characters")
-        else:
-            # Create a result list, defaulting to wrong guesses.
-            result = [wrong_guess for _ in range(len(guess))]
-            # Create a mapping of char -> set of indices
-            chars_remaining = {c: set() for c in word_to_guess}
-            for i, c in enumerate(word_to_guess):
-                chars_remaining[c].add(i)
+	if key not in words:
+		await ctx.send(f"{author.mention} You have not started a Wordle game. Type !wordle to start one.")
+	else:
+		guess: str = ctx.message.content.split(" ", 1)[1].lower()
+		# Validate guesses
+		if len(guess) != 6:
+			await ctx.send(f"{author.mention} Guesses must be 6 characters")
+		elif not guess.isalpha():
+			await ctx.send(f"{author.mention} Guesses must contain only alphabetic characters")
+		else:
+			word_to_guess = words[key]
+			# Create a result list, defaulting to wrong guesses.
+			result = [wrong_guess for _ in range(len(guess))]
+			# Create a mapping of char -> set of indices
+			chars_remaining = {c: set() for c in word_to_guess}
+			for i, c in enumerate(word_to_guess):
+				chars_remaining[c].add(i)
 
-            # First pass: Find all chars in guess that are exactly correct and update result.
-            for i, ch in enumerate(guess):
-                if ch == word_to_guess[i]:
-                    result[i] = correct_guess
-                    # Remove this index from remaining chars
-                    chars_remaining[ch].remove(i)
-                    if len(chars_remaining[ch]) == 0:
-                        del chars_remaining[ch]
+			# First pass: Find all chars in guess that are exactly correct and update result.
+			for i, ch in enumerate(guess):
+				if ch == word_to_guess[i]:
+					result[i] = correct_guess
+					# Remove this index from remaining chars
+					chars_remaining[ch].remove(i)
+					if len(chars_remaining[ch]) == 0:
+						del chars_remaining[ch]
 
-            # Second pass: Find all chars in guess that were missed and still exist in the word to guess.
-            for i, ch in enumerate(guess):
-                if result[i] != correct_guess and ch in chars_remaining:
-                    result[i] = missed_guess
+			# Second pass: Find all chars in guess that were missed and still exist in the word to guess.
+			for i, ch in enumerate(guess):
+				if result[i] != correct_guess and ch in chars_remaining:
+					result[i] = missed_guess
 
-            num_guesses_used += 1
+			guesses_used[key] += 1
 
-            emoji_guess = ''.join((f":regional_indicator_{c}:" for c in guess))
-            guess_str = f'\n{emoji_guess}\n{"".join(result)}'
-            prev_guess_stack.append(guess_str)
-            await ctx.send(
-                f'{author.mention}{"".join(prev_guess_stack)}\n**{MAX_GUESSES - num_guesses_used} guesses remaining**'
-            )
+			emoji_guess = ''.join((f":regional_indicator_{c}:" for c in guess))
+			guess_str = f'\n{emoji_guess}\n{"".join(result)}'
+			prev_guesses[key].append(guess_str)
+			await ctx.send(
+				f'{author.mention}{"".join(prev_guesses[key])}\n**{MAX_GUESSES - guesses_used[key]} guesses remaining**'
+			)
 
-            if guess == word_to_guess:
-                key = author.id
-                current_score = int(db.get(key)) if db.exists(key) else 0
-                db.set(key, current_score + 1)
-                await ctx.send(f'{author.mention} guessed correctly! You now have {current_score + 1} points.')
-                word_to_guess = None
-                print(f'{author.id} now has {current_score + 1 } wordle points.')
-            elif num_guesses_used == MAX_GUESSES:
-                await ctx.send(f"Better luck next time! The word was '{word_to_guess}'")
-                word_to_guess = None
+			if guess == word_to_guess:
+				current_score = int(db.get(key)) if db.exists(key) else 0
+				new_score = current_score + 1
+				db.set(key, new_score)
+				await ctx.send(f'{author.mention} guessed correctly! You now have {new_score} points.')
+				del words[key]
+			elif guesses_used[key] == MAX_GUESSES:
+				await ctx.send(f"Better luck next time! The word was '{word_to_guess}'")
+				del words[key]
 
 
 @bot.command()
 async def clearwordle(ctx: Context):
-    global word_to_guess
-    if word_to_guess is not None:
-        word_to_guess = None
-        await ctx.send("Wordle game canceled.")
-    else:
-        await ctx.send("Wordle game is not in progress.")
+	global words
+
+	author = ctx.author
+	if author.id in words:
+		del words[author.id]
+		await ctx.send(f"{author.mention} Wordle game canceled.")
+	else:
+		await ctx.send(f"{author.mention} You do not have a Wordle game in progress.")
