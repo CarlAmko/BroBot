bec5e85a2135255a537394e4f04dfdda902da4d6
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-07-29 18:19:47 -0400

    Command to check diggities to dms

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord import Guild, Member
 
 import env
-from modules import audio, bot, core, dice, game_price, poll, randomizer, reddit, scheduler, wordle
+from modules import audio, bot, core, dice, game_price, poll, randomizer, reddit, scheduler, wordle, diggity
 from modules.core import get_guild, get_members_channel
 
 
