8cc66f9da35e813215877a550918ed1334e5e7b4
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-07-29 19:25:38 -0400

    Changed format of diggities message

--- modules/diggity.py
+++ modules/diggity.py
@@ -10,5 +10,5 @@ async def diggities(ctx: Context):
     key = ctx.author.id
     current_balance = int(db.get(key)) if db.exists(key) else 0
 
-    await ctx.author.send(f"You have '{current_balance}' diggities.")
+    await ctx.author.send(f"You have **{current_balance} diggities**.")
     
