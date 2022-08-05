368a5d24d1853015032bc0228a7989f1ef6d72c3
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-05 12:14:45 -0400

    Included slots on main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import env
 from modules import audio, bot, core, dice, game_price, poll, randomizer, reddit, scheduler, wordle, diggity
 from modules.fishing import fishing
 from modules.wordle import wordle
+from modules.casino import slot_machine
 from modules.core import get_guild, get_members_channel
 
 
