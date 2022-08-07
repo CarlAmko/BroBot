9bc735ad660d4b4cae4ea9f207baf89a8f7d6755
Carl Amko <carl@carlamko.me>
2022-08-07 06:13:55 -0600

    v1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
 	name='BroBot',
-	version='1.0.4',
+	version='1.0.5',
 	description='Discord bot for the bros.',
 	license="MIT",
 	long_description=long_description,
