bc852e99bbc0c91963be85976a1b593d56aff9b4
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-16 09:32:40 -0400

    Initializing a new pet

--- modules/pets/data/models.py
+++ modules/pets/data/models.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Dict
 from enum import Enum
+import random
 
 from emoji import emojize
 
 
-class Status(Enum):
+class StatusEffects(Enum):
     poisoned = 1
     stunned = 2
     immobile = 3
@@ -13,23 +14,71 @@ class Status(Enum):
     strengthened = 6
 
 
-class StatSet:
-    mgt: int
-    swt: int
-    vit: int
-    smt: int
-    ins: int
+class Stats(Enum):
+    mgt = 0
+    swt = 1
+    vit = 2
+    smt = 3
+    ins = 4
+
+    @property
+    def get_stat_str(self) -> str:
+        if self.value == Stats.mgt.value:
+            return "mgt"
+        elif self.value == Stats.swt.value:
+            return "swt"
+        elif self.value == Stats.vit.value:
+            return "vit"
+        elif self.value == Stats.smt.value:
+            return "smt"
+        else:
+            return "ins"
 
 
 class Pet:
+    emoji: str
     level: int
     xp_to_level: int
     current_xp: int
-    stats: StatSet
+    pet_stats: List[int]
     max_hp: int
     current_hp: int
-    status_effects: List[Status]
+    status_effects: List[StatusEffects]
     armor: int
     shield: int
     weapon: int
     hat: int
+
+    def __init__(self, desired_emoji: str):
+        self.emoji = emojize(f":{desired_emoji}:")
+        self.level = 1
+        self.xp_to_level = 10
+        self.current_xp = 0
+
+        stat_bonuses = [0] * 5
+        stat_bonuses[random.randint(0, 4)] += 2
+        for _ in range(2):
+            stat_bonuses[random.randint(0, 4)] += 1
+
+        i = 0
+        for stat in self.pet_stats:
+            stat = 1 + stat_bonuses[i]
+            i += 1
+
+        self.max_hp = 5 + ((self.pet_stats[Stats.vit.value] - 1) * 3)
+        self.current_hp = self.max_hp
+
+    def stat_inc(self, stat_to_inc: str, amount=1):
+        if stat_to_inc == "mgt":
+            self.pet_stats[Stats.mgt.value] += amount
+        elif stat_to_inc == "swt":
+            self.pet_stats[Stats.swt.value] += amount
+        elif stat_to_inc == "vit":
+            self.pet_stats[Stats.vit.value] += amount
+        elif stat_to_inc == "smt":
+            self.pet_stats[Stats.smt.value] += amount
+        elif stat_to_inc == "ins":
+            self.pet_stats[Stats.ins.value] += amount
+        else:
+            print(f"Invalid Stat {stat_to_inc}")
+            # TODO Error for invalid stat
