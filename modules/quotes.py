fae58daf7df689015cd4c5762cc22726993dcb39
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-21 01:46:30 -0400

    Now caches quote messages from channel for 1 day

--- modules/quotes.py
+++ modules/quotes.py
@@ -6,21 +6,27 @@ import emoji
 from database.db import db
 
 QUOTES_ID = 871496158474821654
-TIME_TO_DELETE = 60
-# TIME_TO_DELETE = 86400
+TIME_TO_DELETE = 86400
 loading_emj = emoji.emojize("<a:loading:734622445910360105>")
 
 
 @bot.command()
 async def quote(ctx: Context):
     current_message = await ctx.send(f"Give me a sec to find something good. {loading_emj}")
-    namespace = f'{QUOTES_ID}:quote'
+    ids_namespace = f'{QUOTES_ID}:quote:flag'
 
-    if not db.exists(namespace) == 1:
+    if not db.exists(ids_namespace) == 1:
         quotes = await bot.get_channel(QUOTES_ID).history(limit=None).flatten()
-        db.setex(namespace, TIME_TO_DELETE)
-    else:
-        quotes = db.get(namespace)
+        quote_ids = []
+        for current_quote in quotes:
+            quote_namespace = f'{current_quote.id}:quote'
+            db.setex(quote_namespace, TIME_TO_DELETE, str(current_quote.content))
+            quote_ids.append(current_quote.id)
 
-    random_quote = random.choice(quotes)
-    await current_message.edit(content=(random_quote.content + f"\n{random_quote.jump_url}"))
+        db.setex(ids_namespace, TIME_TO_DELETE, str(quote_ids))
+    else:
+        quote_ids = eval(db.get(ids_namespace))
+
+    random_quote = random.choice(quote_ids)
+    selected_quote = str(db.get(f'{random_quote}:quote'))[2:-1]
+    await current_message.edit(content=selected_quote)
