ee9bdd2e26d4ca22a922e8b1988552d2269cd897
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-22 15:56:23 -0400

    Cleaning up branch

--- modules/casino/data/models.py
+++ modules/casino/data/models.py
@@ -5,18 +5,6 @@ from typing import List, Optional
 
 from emoji import emojize
 
-JACKPOT_BASE = 1000
-
-
-class SlotMachineSizes(Enum):
-    sml = 1
-    med = 25
-    lrg = 100
-
-    @property
-    def generate_key(self) -> str:
-        return "777" + str(self.value)
-
 
 class Suit(Enum):
 	SPADES = 0
