6ddd4c5d5aa686dcdf0be1689a86f1f0f1b011fb
Carl Amko <carl@carlamko.me>
2022-08-23 09:07:39 -0600

    These were missing

--- modules/casino/data/models.py
+++ modules/casino/data/models.py
@@ -8,6 +8,16 @@ from emoji import emojize
 JACKPOT_BASE = 1000
 
 
+class SlotMachineSizes(Enum):
+	sml = 1
+	med = 25
+	lrg = 100
+
+	@property
+	def generate_key(self) -> str:
+		return "777" + str(self.value)
+
+
 class Suit(Enum):
 	SPADES = 0
 	HEARTS = 1
