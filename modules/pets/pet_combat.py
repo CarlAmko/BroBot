5bd5e13d8dd50ae88df6902dced881c9216d1df7
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-23 01:41:44 -0400

    Optimized Dueling battlefield
    Now only uses 3 message for full battlefield, but must now use small form of emojis.
    No longer builds empty battlefield before gathering members for duel.

--- modules/pets/pet_combat.py
+++ modules/pets/pet_combat.py
@@ -5,12 +5,14 @@ from enum import Enum
 
 import emoji
 from discord.ext.commands import Context
-from discord import Message
+from discord import Message, Member
 from modules import bot
 from modules.pets.data import db_pets
 from modules.pets.data.models import Pet, StatusEffects
 
 DUELING = 1011161175163142175
+BATTLEFIELD_SIZE = 10
+ROWS_PER_MESSAGE = 5
 
 nothing_emj = emoji.emojize("<:nothing:1004150080779059381>")
 grid_emj = emoji.emojize("<:Grid:1009500126177407016>")
@@ -55,34 +57,20 @@ class Duel:
 
 class PetInBattle:
     pet: Pet
+    owner: Member
     coords: [int] * 2
     status_effects: List[StatusEffects]
     initiative: int
 
-    def __init__(self, new_pet: Pet):
+    def __init__(self, new_pet: Pet, current_owner: Member):
         self.pet = new_pet
+        self.owner = current_owner
 
 
 current_duel = Duel
 
 
-async def print_battlefield(ctx: Context) -> List[Message]:
-    messages = []
-    battlefield = ""
-    battlefield += nothing_emj
-    for emj in letters_emjs:
-        battlefield += emj
-    messages.append(await ctx.send(f"{battlefield}"))
-    for emj in number_emjs:
-        battlefield = ""
-        battlefield += emj
-        for _ in range(10):
-            battlefield += grid_emj
-        messages.append(await ctx.send(f"{battlefield}"))
-    return messages
-
-
-async def add_to_battlefield(ctx: Context, messages: List[Message], pets: List[PetInBattle], duel_type: int):
+async def set_starting_positions(pets: List[PetInBattle], duel_type: int):
     if duel_type == DuelType.ffa.value:
         starting_coords = [[0, 0], [0, 9], [9, 0], [9, 9]]
         if len(pets) == 2:
@@ -96,25 +84,51 @@ async def add_to_battlefield(ctx: Context, messages: List[Message], pets: List[P
         for i in range(len(pets)):
             pets[i].coords = starting_coords[i]
 
-    await update_battlefield(ctx, messages, pets)
 
+async def update_battlefield(ctx: Context, messages: List[Message], pets: List[PetInBattle], duel_type: int) -> List[Message]:
+    if len(messages) == 0:
+        header = ""
+        if duel_type == DuelType.ffa.value:
+            header += "Free-for-all duel with:  "
+            for pet in pets:
+                header += f"{pet.owner.mention}  "
+        else:
+            header += "Team duel with: "
+            for i in range (2):
+                header += f"{pets[i].owner.mention}  "
+            header += "VS  "
+            for i in range (2, len(pets)):
+                header += f"{pets[i].owner.mention}  "
 
-async def update_battlefield(ctx: Context, messages: List[Message], pets: List[PetInBattle]):
-    for i in range(len(messages) - 1):
-        pets_in_row = []
-        for pet in pets:
-            if pet.coords[0] == i:
-                pets_in_row.append(pet)
-        row_message = f"{number_emjs[i]}"
-        for j in range(10):
-            occupied = False
-            for pet in pets_in_row:
-                if pet.coords[1] == j:
-                    row_message += emoji.emojize(pet.pet.pet_emoji)
-                    occupied = True
-            if not occupied:
-                row_message += grid_emj
-        await messages[i + 1].edit(content=row_message)
+        header += f"\n{nothing_emj}"
+        for emj in letters_emjs:
+            header += emj
+        messages.append(await ctx.send(header))
+
+    for i in range(2):
+        row_messages = ""
+        for j in range(ROWS_PER_MESSAGE):
+            pets_in_row = []
+            for pet in pets:
+                if pet.coords[0] == j + (ROWS_PER_MESSAGE * i):
+                    pets_in_row.append(pet)
+            row_messages += f"{number_emjs[j + (ROWS_PER_MESSAGE * i)]}"
+            for k in range(10):
+                occupied = False
+                for pet in pets_in_row:
+                    if pet.coords[1] == k:
+                        row_messages += emoji.emojize(pet.pet.pet_emoji)
+                        occupied = True
+                if not occupied:
+                    row_messages += grid_emj
+            row_messages += "\n"
+
+        if len(messages) <= 2:
+            messages.append(await ctx.send(row_messages))
+        else:
+            await messages[i + 1].edit(content=row_messages)
+
+    return messages
 
 
 @bot.command()
@@ -143,9 +157,8 @@ async def duel(ctx: Context):
     duel_is_active = True
     current_duel.wager = wager
     diggities = "diggity" if wager == 1 else "diggities"
-    battlefield_messages = await print_battlefield(ctx)
     hosts_pet = db_pets.get_pet(author_id)
-    pets_in_battle = [PetInBattle(hosts_pet)]
+    pets_in_battle = [PetInBattle(hosts_pet, author)]
     duel_joined = False
 
     duel_join_msg = await ctx.send(f"{author.mention} has opened a duel for **{wager} {diggities}**!\n"
@@ -154,7 +167,6 @@ async def duel(ctx: Context):
     duel_setup_text = duel_join_msg.content
 
     cannot_join = [author_id]
-    duelists = []
     while len(pets_in_battle) < 4:
         try:
             reply = await bot.wait_for('message', timeout=60.0)
@@ -190,8 +202,7 @@ async def duel(ctx: Context):
                                 cannot_join.append(reply.author.id)
                                 duel_joined = True
                                 joiners_pet = db_pets.get_pet(reply.author.id)
-                                pets_in_battle.append(PetInBattle(joiners_pet))
-                                duelists.append(reply.author)
+                                pets_in_battle.append(PetInBattle(joiners_pet, reply.author))
                                 new_msg_text = duel_setup_text + f"\n{reply.author.mention} has joined the duel."
                                 await duel_join_msg.edit(content=(new_msg_text + f"\nSend \"**start**\" to start the "
                                                                                  f"duel now."))
@@ -235,8 +246,8 @@ async def duel(ctx: Context):
 
     if duel_type == DuelType.team.value:
         team_select_text = f"{author.mention}, enter the number of user you want to team with or send \"random\"\n"
-        for i in range(len(duelists)):
-            team_select_text += f"**{i + 1}:** {duelists[i].mention}\n"
+        for i in range(len(pets_in_battle) - 1):
+            team_select_text += f"**{i + 1}:** {pets_in_battle[i + 1].owner.mention}\n"
         team_select_text += "You have 30 seconds to select or else it will be random."
         team_select_msg = await ctx.send(team_select_text)
 
@@ -246,7 +257,7 @@ async def duel(ctx: Context):
                 reply = await bot.wait_for('message', timeout=30.0)
             except asyncio.TimeoutError:
                 type_not_selected_msg = await ctx.send(f"{author.mention} Times Up!")
-                host_teammate = random.randint(1, len(duelists))
+                host_teammate = random.randint(1, len(pets_in_battle) - 1)
                 return
             else:
                 if reply.author.id == author_id:
@@ -255,9 +266,11 @@ async def duel(ctx: Context):
                     elif reply.content == "2":
                         host_teammate = 2
                     elif reply.content == "3":
-                        if not len(duelists) > 3:
+                        if not (len(pets_in_battle) - 1) > 3:
                             continue
                         host_teammate = 3
+                    elif reply.content == "random":
+                        host_teammate = random.randint(1, len(pets_in_battle) - 1)
                 if reply.channel.id == DUELING:
                     await reply.delete()
 
@@ -266,16 +279,17 @@ async def duel(ctx: Context):
 
     history = await bot.get_channel(ctx.channel.id).history(limit=None).flatten()
     for msg in history:
-        if msg not in battlefield_messages:
-            await msg.delete()
+        await msg.delete()
 
-    await add_to_battlefield(ctx, battlefield_messages, pets_in_battle, duel_type)
+    battlefield_msg = []
+    await set_starting_positions(pets_in_battle, duel_type)
+    battlefield_msg = await update_battlefield(ctx, battlefield_msg, pets_in_battle, duel_type)
 
     # TODO Remove this
     final_msg = await ctx.send("Dueling is under construction. This channel will empty itself in 30 seconds.")
     await asyncio.sleep(30)
     await final_msg.delete()
-    for msg in battlefield_messages:
+    for msg in battlefield_msg:
         await msg.delete()
 
     duel_is_active = False
