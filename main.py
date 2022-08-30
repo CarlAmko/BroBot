4f0ba2e7fa44164e36cea743a960ca07c5d2b45c
Cinderglow <49967736+smaschin@users.noreply.github.com>
2022-08-29 20:11:01 -0400

    Made plinko and plinkolite boards
    Emoji lists may be useful to be stored on a library file or something

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from modules.core import get_guild, get_members_channel
 
 from modules import bot, core, admin, lib
 if not env.BETA:
-	from modules import audio, dice, game_price, poll, randomizer, reddit, scheduler, diggity, quotes
+	from modules import audio, dice, game_price, poll, randomizer, reddit, scheduler, diggity, quotes, plinko
 	from modules.fishing import fishing
 	from modules.casino import blackjack, slot_machine
 	from modules.wordle import wordle
